2aa9035e3a5629806459554af66a20d8aebd34fd
okrishnakumarmehta <youremail@example.com>
2025-03-31 23:10:56 +0530

    Updated project

--- main.c
+++ main.c
@@ -1,6 +1,51 @@
-#include <stdio.h>
+#include<stdio.h>
+int main()
+{
+    int num1, num2, choice;
+    float result;
 
-int main(void) {
-    printf("Hello, World!\n");
+    printf("Simple Calculator\n");
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("Choose an operation:\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+        case 1:
+            result = num1 + num2;
+        printf("Result: %.2f\n", result);
+        break;
+        case 2:
+            result = num1 - num2;
+        printf("Result: %.2f\n", result);
+        break;
+        case 3:
+            result = num1 * num2;
+        printf("Result: %.2f\n", result);
+        break;
+        case 4:
+            if (num2 != 0)
+            {
+                result = (float)num1 / num2;
+                printf("Result: %.2f\n", result);
+            }
+            else
+            {
+                printf("Error: Division by zero is not allowed.\n");
+            }
+        break;
+        default:
+            printf("Invalid choice.\n");
+    }
+
     return 0;
 }
